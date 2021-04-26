d528a64839983eebf31e0229a7f601066cbb414c
ulysse <ulysse.yadan@gmail.com>
2021-04-26 16:09:11 +0200

    Formulaire de création et d'édition d'un paysage (Request, Form) + navigation basique entre les pages. A faire : suppression d'un paysage + esthétique (css)

--- projet_panterest/src/Controller/PaysageController.php
+++ projet_panterest/src/Controller/PaysageController.php
@@ -6,12 +6,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\PaysagesRepository;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use App\Entity\Paysages;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Form\PaysageType;
+
 
 class PaysageController extends AbstractController
 {
     /**
-     * @Route("/", name="app_home")
+     * @Route("/", name="app_home", methods="GET")
      */
     public function index(PaysagesRepository $PaysagesRepository): Response
     {
@@ -21,10 +25,52 @@ class PaysageController extends AbstractController
         return $this->render('pins/index.html.twig', compact('paysages'));
     }
      /**
-     * @Route("/paysage/{id<[0-9]+>}", name="paysages_app")
+     * @Route("/paysage/{id<[0-9]+>}", name="paysages_app", methods="GET")
      */
     public function show(Paysages $paysage): Response{
        
        return $this->render('pins/show.html.twig', compact('paysage'));
     }
+    /**
+     * @Route("/paysages/create", name="create_app", methods={"GET","POST"})
+     */
+    public function create(Request $request, EntityManagerInterface $em): Response
+    {
+        $paysages = new Paysages;
+
+        $form = $this->createForm(PaysageType::class, $paysages);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($paysages);
+            $em->flush();
+
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('pins/create.html.twig', [
+            'monFormulaire' => $form->CreateView()
+        ]);
+    }
+    /**
+     * @Route("/paysages/{id<[0-9]+>}/edit", name="edit_app", methods={"GET","POST"})
+     */
+    public function edit(Paysages $paysages, Request $request, EntityManagerInterface $em): Response
+    {
+        $form = $this->createForm(PaysageType::class, $paysages);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+
+           return $this->redirectToRoute('app_home');
+        }
+        return $this->render('pins/edit.html.twig', [
+            'paysages' => $paysages,
+            'monFormulaire' => $form->createView()
+        ]);
+       
+    } 
 }
